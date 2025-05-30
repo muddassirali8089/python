9a107f7e0ac79b166b7d5ce6c53d237b04d53cdf
muddassirali8089 <114619571+muddassirali8089@users.noreply.github.com>
2025-05-30 08:43:29 +0500

    accessing the class variables and updation
    accessing the class variables and updation

--- OOP/OOPBASICS.py
+++ OOP/OOPBASICS.py
@@ -4,11 +4,4 @@ from car import Car
 
 car1 = Car("bmw" , 2023 , "black" , False)
 
-print(car1.model)
-print(car1.year)
-print(car1.color)
-print(car1.forSale)
-
-
-car1.drive()
-car1.stop()
+car1.describe()
--- OOP/car.py
+++ OOP/car.py
@@ -1,10 +1,13 @@
 
 class Car:
+
+    carCount = 0
     def __init__(self , model , year , color , forSale):
         self.model = model
         self.year = year
         self.color = color
         self.forSale = forSale
+        Car.carCount+=1
 
 
     def drive(self):
@@ -12,5 +15,8 @@ class Car:
     
     def stop(self):
         print("you stop the car...")
+
+    def describe(self):
+        print(f"details of car : \n model {self.model} \n year : {self.year}\n Color : {self.color}\n Total Cars : {Car.carCount} \n")
 
         
